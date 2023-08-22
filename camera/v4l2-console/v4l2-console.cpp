c1140b8da5998c5c25dd7bfe062d92582e789dbd
JC Li <jincheng.li@gmail.com>
2023-08-22 10:48:07 -0700

    Add remap() kernel to v4l2-console application.

--- camera/v4l2-console/v4l2-console.cpp
+++ camera/v4l2-console/v4l2-console.cpp
@@ -23,9 +23,11 @@
 #include "v4l2Camera.h"
 #include "glDisplay.h"
 #include "NvAnalysis.h"
+#include "mapxpy.h"
 
 #include <stdio.h>
 #include <signal.h>
+#include <cassert>
 #include <cuda.h>
 
 
@@ -33,155 +35,178 @@ bool signal_recieved = false;
 
 void sig_handler(int signo)
 {
-	if( signo == SIGINT )
-	{
-		printf("received SIGINT\n");
-		signal_recieved = true;
-	}
+    if( signo == SIGINT )
+    {
+        printf("received SIGINT\n");
+        signal_recieved = true;
+    }
 }
 
 
 
 int main( int argc, char** argv )
 {
-	printf("v4l2-console\n  args (%i):  ", argc);
-	
-	/*
-	 * verify parameters
-	 */
-	for( int i=0; i < argc; i++ )
-		printf("%i [%s]  ", i, argv[i]);
-		
-	printf("\n");
-	
-	if( argc < 2 )
-	{
-		printf("v4l2-console:  0 arguments were supplied.\n");
-		printf("usage:  v4l2-console <filename>\n");
-		printf("      ./v4l2-console /dev/video0\n");
-		
-		return 0;
-	}
-	
-	const char* dev_path = argv[1];
-	printf("v4l2-console:   attempting to initialize video device '%s'\n\n", dev_path);
-	
-	if( signal(SIGINT, sig_handler) == SIG_ERR )
-		printf("\ncan't catch SIGINT\n");
+    printf("v4l2-console\n  args (%i):  ", argc);
+    
+    /*
+     * verify parameters
+     */
+    for( int i=0; i < argc; i++ )
+        printf("%i [%s]  ", i, argv[i]);
+        
+    printf("\n");
+    
+    if( argc < 2 )
+    {
+        printf("v4l2-console:  0 arguments were supplied.\n");
+        printf("usage:  v4l2-console <filename>\n");
+        printf("      ./v4l2-console /dev/video0\n");
+        
+        return 0;
+    }
+    
+    const char* dev_path = argv[1];
+    printf("v4l2-console:   attempting to initialize video device '%s'\n\n", dev_path);
+    
+    if( signal(SIGINT, sig_handler) == SIG_ERR )
+        printf("\ncan't catch SIGINT\n");
 
-	/*
-	 * create the camera device
-	 */
-	v4l2Camera* camera = v4l2Camera::Create(dev_path);
-	
-	if( !camera )
-	{
-		printf("\nv4l2-console:  failed to initialize video device '%s'\n", dev_path);
-		return 0;
-	}
-	
-	printf("\nv4l2-console:  successfully initialized video device '%s'\n", dev_path);
-	printf("    width:  %u\n", camera->GetWidth());
-	printf("   height:  %u\n", camera->GetHeight());
-	printf("    depth:  %u (bpp)\n", camera->GetPixelDepth());
-	
+    /*
+     * create the camera device
+     */
+    v4l2Camera* camera = v4l2Camera::Create(dev_path);
+    
+    if( !camera )
+    {
+        printf("\nv4l2-console:  failed to initialize video device '%s'\n", dev_path);
+        return 0;
+    }
+    
+    printf("\nv4l2-console:  successfully initialized video device '%s'\n", dev_path);
+    printf("    width:  %u\n", camera->GetWidth());
+    printf("   height:  %u\n", camera->GetHeight());
+    printf("    depth:  %u (bpp)\n", camera->GetPixelDepth());
+    
 
-	/*
-	 * create openGL window
-	 */
-	glDisplay* display = glDisplay::Create("", camera->GetPitch(), camera->GetHeight(), 0.f, 0.f, 0.f, 0.f);
-	
-	if( !display )
-	{
-		printf("\nv4l2-display:  failed to create openGL display\n");
-		return 0;
-	}
+    /*
+     * create openGL window
+     */
+    glDisplay* display = glDisplay::Create("", camera->GetPitch(), camera->GetHeight(), 0.f, 0.f, 0.f, 0.f);
+    
+    if( !display )
+    {
+        printf("\nv4l2-display:  failed to create openGL display\n");
+        return 0;
+    }
 
-	
-	/*
-	 * start streaming
-	 */
-	if( !camera->Open() )
-	{
-		printf("\nv4l2-console:  failed to open camera '%s' for streaming\n", dev_path);
-		return 0;
-	}
-	
-	printf("\nv4l2-console:  camera '%s' open for streaming\n", dev_path);
+    
+    /*
+     * start streaming
+     */
+    if( !camera->Open() )
+    {
+        printf("\nv4l2-console:  failed to open camera '%s' for streaming\n", dev_path);
+        return 0;
+    }
+    
+    printf("\nv4l2-console:  camera '%s' open for streaming\n", dev_path);
 
-	// Device memory for CUDA processing.
-	uint8_t* img_dev = nullptr;
-	if( CUDA(cudaMalloc(&img_dev, camera->GetPitch()*camera->GetHeight())) )
-		printf("cudaMalloc failed!\n");
-	
-	
-	uint32_t img_cnt = 0;
-	while( !signal_recieved )
-	{
-		uint8_t* img = (uint8_t*)camera->Capture(500);
-		
-		if( !img )
-		{
-			//printf("got NULL image from camera capture\n");
-			continue;
-		}
-		else
-		{
-			//printf("recieved new video frame\n");
+    assert(camera->GetPitch() == IMG_W*2);
+    assert(camera->GetHeight() == IMG_H);
+    int height = IMG_H;
+    int width = IMG_W;
+    size_t sizeOfImage = width * height;
 
-			if (img_cnt==0) {
-				FILE *fout = fopen("frame.raw", "wb");
-				fwrite(img, camera->GetPitch()*camera->GetHeight(), 1, fout);
-				fclose(fout);
-			}
+    // Device memory for CUDA processing.
+    uint8_t* img_dev = nullptr;
+    float *mapxDevPtr, *mapyDevPtr;
+    if( CUDA(cudaMalloc(&img_dev, 2*sizeOfImage * sizeof(uint8_t))) )
+        printf("cudaMalloc img_dev failed!\n");
+    if( CUDA(cudaMalloc(&mapxDevPtr, sizeOfImage * sizeof(float))) )
+        printf("cudaMalloc mapxDevPtr failed!\n");
+    if( CUDA(cudaMalloc(&mapyDevPtr, sizeOfImage * sizeof(float))) )
+        printf("cudaMalloc mapyDevPtr failed!\n");
 
-			img_cnt++;
+    // Copy mapx mapy to device mem.
+    cudaMemcpy(mapxDevPtr, mapx, sizeOfImage * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(mapyDevPtr, mapy, sizeOfImage * sizeof(float), cudaMemcpyHostToDevice);
 
-			// Copy img to device
-			printf("Copy img to img_dev.\n");
-			CUDA(cudaMemcpy(img_dev, img, camera->GetPitch()*camera->GetHeight(), cudaMemcpyHostToDevice));
 
-			// CUDA proc
-        	decoupleLR((CUdeviceptr) img_dev, camera->GetPitch());
+    uint32_t img_cnt = 0;
+    while( !signal_recieved )
+    {
+        uint8_t* img = (uint8_t*)camera->Capture(500);
+        
+        if( !img )
+        {
+            //printf("got NULL image from camera capture\n");
+            continue;
+        }
+        else
+        {
+            //printf("recieved new video frame\n");
 
-			// update display
-			if( display != NULL )
-			{
-				//display->Render((uint8_t*)img, camera->GetWidth(), camera->GetHeight(), IMAGE_RGBA8);
-				display->RenderOnce((uint8_t*)img_dev, camera->GetPitch(), camera->GetHeight(), IMAGE_GRAY8, 0, 0);
+            if (img_cnt==0) {
+                FILE *fout = fopen("frame.raw", "wb");
+                fwrite(img, camera->GetPitch()*camera->GetHeight(), 1, fout);
+                fclose(fout);
+            }
 
-				// update status bar
-				char str[256];
-				sprintf(str, "v4l2-console (%ux%u) | %.0f FPS", camera->GetWidth(), camera->GetHeight(), display->GetFPS());
-				display->SetTitle(str);	
+            img_cnt++;
 
-				// check if the user quit
-				if( display->IsClosed() )
-					signal_recieved = true;
-			}
+            cudaMemcpy(img_dev, img, 2*sizeOfImage*sizeof(uint8_t), cudaMemcpyHostToDevice);
+            printf("Copied img to img_dev.\n");
 
-		}
-			
-	}
-	
-	
-	/*
-	 * shutdown the camera device
-	 */
-	if( display != NULL )
-	{
-		delete display;
-		display = NULL;
-	}
+            // CUDA proc
+            decoupleLR((CUdeviceptr) img_dev, width*2);
+            cudaDeviceSynchronize();
+            remap(img_dev, img_dev + width, mapxDevPtr, mapyDevPtr, width*2);
+            cudaDeviceSynchronize();
 
-	printf("\nv4l2-console:  un-initializing video device '%s'\n", dev_path);
-	if( camera != NULL )
-	{
-		delete camera;
-		camera = NULL;
-	}
-	
-	printf("v4l2-console:  video device '%s' has been un-initialized.\n", dev_path);
-	printf("v4l2-console:  this concludes the test of video device '%s'\n", dev_path);
-	return 0;
+            printf("CUDA kernels done.\n");
+
+            // update display
+            if( display != NULL )
+            {
+                //display->Render((uint8_t*)img, camera->GetWidth(), camera->GetHeight(), IMAGE_RGBA8);
+                display->RenderOnce((uint8_t*)img_dev, camera->GetPitch(), camera->GetHeight(), IMAGE_GRAY8, 0, 0);
+
+                // update status bar
+                char str[256];
+                sprintf(str, "v4l2-console (%ux%u) | %.0f FPS", camera->GetWidth(), camera->GetHeight(), display->GetFPS());
+                display->SetTitle(str); 
+
+                // check if the user quit
+                if( display->IsClosed() )
+                    signal_recieved = true;
+            }
+
+        }
+            
+    }
+    
+    // Free cuda allocations.
+    cudaFree(img_dev);
+    cudaFree(mapxDevPtr);
+    cudaFree(mapyDevPtr);
+    
+    /*
+     * shutdown the camera device
+     */
+    if( display != NULL )
+    {
+        delete display;
+        display = NULL;
+    }
+
+    printf("\nv4l2-console:  un-initializing video device '%s'\n", dev_path);
+    if( camera != NULL )
+    {
+        delete camera;
+        camera = NULL;
+    }
+    
+    printf("v4l2-console:  video device '%s' has been un-initialized.\n", dev_path);
+    printf("v4l2-console:  this concludes the test of video device '%s'\n", dev_path);
+    return 0;
 }
